cfac6f61d12a42f0fdf46895b4da33c6fc3209f0
pavelpetrov47 <pavel.petrov@blankfactor.com>
2024-01-05 14:41:41 +0200

    Java project

---
+++ AI_CV_JAVA/src/test/java/com/example/AI_CV_JAVA/AiCvJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_CV_JAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCvJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
