9c4e0d559beccf4593128b33a72f5591ae2a6890
nataliya-nikolova <nataliya.nikolova@blankfacktor.com>
2024-01-09 11:41:59 +0200

    changes to the ai

--- AI_CV_Python/main.py
+++ AI_CV_Python/main.py
@@ -8,20 +8,20 @@ def home():
     return "Running"
 
 
-@app.route('/send')
-def send_json_to_java():
-    data_json = {"name": "John", "age": 30}
-    publish_message_to_rabbitmq(data_json)
+# @app.route('/send')
+# def send_json_to_java():
+#     data_json = {"name": "John", "age": 30}
+#     publish_message_to_rabbitmq(data_json)
 
-    return "Sending"
+#     return "Sending"
 
 
-@app.route('/listen')
-def recieve_bytearray_from_java():
-    start_listening()
-    return "Listening..."
+# @app.route('/listen')
+# def recieve_bytearray_from_java():
+#     start_listening()
+#     return "Listening..."
 
 
 if __name__ == '__main__':
-    # start_listening()
+    start_listening()
     app.run(port=5001)
