d03f67c95e42c7ac55128c188a5ad8995cecae3a
pavelpetrov47 <pavel.petrov@blankfactor.com>
2024-01-10 10:31:33 +0200

    add multipartController

---
+++ AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/controller/MultipartController.java
@@ -0,0 +1,34 @@
+package com.example.AI_CV_JAVA.controller;
+import com.example.AI_CV_JAVA.service.PdfService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+
+@RestController
+@RequestMapping("/pdf") // Define the base path here
+@RequiredArgsConstructor
+public class MultipartController {
+
+    private final PdfService pdfService;
+
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            pdfService.upload(file);
+        } catch (IOException e) {
+            ResponseEntity.notFound().build();
+        }
+       return ResponseEntity.ok("Ok");
+
+    }
+}
+
