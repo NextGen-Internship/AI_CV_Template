55ac9c1a9a7cc20b7a688a3b854bd4114f4fc107
pavelpetrov47 <152274719+pavelpetrov47@users.noreply.github.com>
2024-01-18 11:08:51 +0200

    Update MultipartController.java spaces fixed

--- AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/controller/MultipartController.java
+++ AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/controller/MultipartController.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
-
-
 @RestController
 @RequestMapping("/pdf") // Define the base path here
 @RequiredArgsConstructor
 public class MultipartController {
 
     private final PdfService pdfService;
-
-
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
@@ -28,7 +23,6 @@ public class MultipartController {
             ResponseEntity.notFound().build();
         }
        return ResponseEntity.ok("Ok");
-
     }
 }
 
