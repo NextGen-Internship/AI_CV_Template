c78d662e36979891ec6a95b87534aba01a106e96
kaloyan-st <kaloyan.stoyanov@blankfactor.com>
2024-01-22 15:45:14 +0200

    Remove autowire

--- AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/controller/UserController.java
+++ AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/controller/UserController.java
@@ -1,9 +1,8 @@
 package com.example.AI_CV_JAVA.controller;
 
-
 import com.example.AI_CV_JAVA.DTO.UserDTO;
 import com.example.AI_CV_JAVA.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
+    private final UserService userService;
 
     @GetMapping("/users/{id}")
     private ResponseEntity<UserDTO> getUserDetails(@PathVariable("id") int id) {
