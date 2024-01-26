db44af91c481f1ed0e15dffccddc379e43055a31
pavelpetrov47 <pavel.petrov@blankfactor.com>
2024-01-26 11:54:09 +0200

    change react config

--- AI_CV_React/frontend/vite.config.js
+++ AI_CV_React/frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/pdf': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       },
     },
