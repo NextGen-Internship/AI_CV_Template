ef1850605f98b40ae15da1ca1db2e6110aab7104
pavelpetrov47 <pavel.petrov@blankfactor.com>
2024-01-29 09:46:10 +0200

    removed empty lines, comments and constructors

--- AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/DTO/EducationDto.java
+++ AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/DTO/EducationDto.java
@@ -1,12 +1,7 @@
 package com.example.AI_CV_JAVA.DTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class EducationDto {
     private long id;
--- AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/DTO/PersonDto.java
+++ AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/DTO/PersonDto.java
@@ -1,14 +1,10 @@
 package com.example.AI_CV_JAVA.DTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class PersonDto {
     private String name;
     private String summary;
