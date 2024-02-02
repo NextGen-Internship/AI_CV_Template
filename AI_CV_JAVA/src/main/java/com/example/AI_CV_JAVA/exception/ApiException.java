110342836d0fdb42a43e63902df50f3483b13044
kaloyan-st <kaloyan.stoyanov@blankfactor.com>
2024-02-02 17:51:47 +0200

    Error Handling

---
+++ AI_CV_JAVA/src/main/java/com/example/AI_CV_JAVA/exception/ApiException.java
@@ -0,0 +1,27 @@
+package com.example.AI_CV_JAVA.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public class ApiException {
+
+    @Getter
+    private final String message;
+
+    @Getter
+    private final HttpStatus httpStatus;
+
+    @Getter
+    private final ZonedDateTime timestamp;
+
+    public ApiException(String message,
+                        HttpStatus httpStatus,
+                        ZonedDateTime timestamp) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.timestamp = timestamp;
+    }
+
+}
