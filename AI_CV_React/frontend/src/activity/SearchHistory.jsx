7c6d055254e5db09a185cbfc4045f2bceec77ab3
pavelpetrov47 <152274719+pavelpetrov47@users.noreply.github.com>
2024-02-14 10:52:31 +0200

    donate,history - react (#34)
    * donate,history - react
    
    * updated
    
    * Update Navbar.jsx
    
    * Update Navbar.jsx
    
    * Update SearchHistory.jsx
    
    * Update UploadHistory.jsx
    
    * prettier

---
+++ AI_CV_React/frontend/src/activity/SearchHistory.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const SearchHistory = () => {
+  const [searchHistory, setSearhHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const storedToken = localStorage.getItem("jwtToken");
+
+  const fetchSearchHistory = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/activity/searched",
+        {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        }
+      );
+      setSearhHistory(JSON.stringify(response.data));
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <p onClick={fetchSearchHistory}>Search History</p>
+    </div>
+  );
+};
+
+export default SearchHistory;
--- AI_CV_React/frontend/src/components/PdfUpload.jsx
+++ AI_CV_React/frontend/src/components/PdfUpload.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PdfUpload = ({ onUpload }) => {
   const [selectedFile, setSelectedFile] = useState(null);
