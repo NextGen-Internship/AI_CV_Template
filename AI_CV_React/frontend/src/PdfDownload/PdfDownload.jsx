27a02bba6732e3444b4752898bc22008fa479c3c
kaloyan-st <kaloyan.stoyanov@blankfactor.com>
2024-02-20 15:43:57 +0200

    Changed ui design of upload and search history

--- AI_CV_React/frontend/src/PdfDownload/PdfDownload.jsx
+++ AI_CV_React/frontend/src/PdfDownload/PdfDownload.jsx
@@ -197,7 +197,7 @@ const PdfDownload = ({ email }) => {
             clearInput={clearInput}
           />
           {emailError && <p className="error-message">{emailError}</p>}
-          <div className="section-label-download">Download PDF:</div>
+          {/* <div className="section-label-download">Download PDF:</div> */}
           <button className="btn-download" onClick={handlePrint}>
             Download Pdf
           </button>
